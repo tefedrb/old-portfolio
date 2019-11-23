a164975e81d9fe07872da0754c5ba77649660fda
tefedrb-enterprise <tefebell@gmail.com>
2019-11-22 23:21:46 -0500

    adding firebase config files

---
+++ 04_JS/firebase-config.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCFAA05omIaYANIQrtTbSKttPdSe6IEwww",
+    authDomain: "sending-email-f1fa6.firebaseapp.com",
+    databaseURL: "https://sending-email-f1fa6.firebaseio.com",
+    projectId: "sending-email-f1fa6",
+    storageBucket: "sending-email-f1fa6.appspot.com",
+    messagingSenderId: "362194574991",
+    appId: "1:362194574991:web:87d572c3f3d7519f655afa",
+    measurementId: "G-D3VVP0D9Y4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
